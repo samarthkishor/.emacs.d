b06f47ae2edc96aebb4233ef4f035408e86d56eb
Samarth Kishor <samarthkishor1@gmail.com>
2018-07-04 10:32:22 -0400

    Made the titlebar match the Emacs theme

--- configuration.el
+++ configuration.el
@@ -67,6 +67,11 @@
 
 (setq custom-safe-themes t)
 
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(setq ns-use-proxy-icon  nil)
+(setq frame-title-format nil)
+
 (setq-default inhibit-startup-screen t)
 
 (display-time-mode t)
